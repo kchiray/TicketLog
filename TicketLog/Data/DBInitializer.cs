f018a075966bb05c5376e316a2a9ae898efd82d9
kchiray <33226274+kchiray@users.noreply.github.com>
2017-11-15 06:53:10 -0500

    inital commit!

---
+++ TicketLog/Data/DBInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketLog.Models;
+
+namespace TicketLog.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            //Look for any tickets
+            context.Database.EnsureCreated();
+            if (context.Tickets.Any())
+            {
+                return; //DB has been seeded
+            }
+
+            var tickets = new Ticket[]
+            {
+                new Ticket {ID=1, Issue="Apply Button", Description="Apply button not working",
+                            Severity ="Threat Level Midnight", TimesOccured=58, TicketDate=DateTime.Parse("2017-11-14") }
+            };
+            foreach (Ticket t in tickets)
+            {
+                context.Tickets.Add(t);
+            } context.SaveChanges();
+            {
+
+            }
+            //    ID { get; set; 
+            //    Time TicketDate
+            //    ng Issue
+            //{
+            //    get;
+            //    ng Description
+            //    TimesOccured
+            //    {
+            //        ng Severity {
+            //            g
+            //    {
+        }
+    }
+}
---
+++ TicketLog/Models/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketLog.Models
+{
+    public class Ticket
+    {
+        public int ID { get; set; }
+        public DateTime TicketDate { get; set; }
+        public string Issue { get; set; }
+        public string Description { get; set; }
+        public int TimesOccured { get; set; }
+        public string Severity { get; set; }
+    }
+}
