496200e224cc47612664b57e8f879c653110c9d4
kchiray <kchiray@my.yorku.ca>
2017-11-21 06:24:43 -0500

    removed time, changed submissiondate format

--- TicketLog/Models/Ticket.cs
+++ TicketLog/Models/Ticket.cs
@@ -8,7 +8,7 @@ namespace TicketLog.Models
     public class Ticket
     {           
         public int ID { get; set; }        
-        public DateTime SubmissionDate { get { return DateTime.Now.Date; } }
+        public String SubmissionDate { get { return DateTime.Now.ToString("dd/mm/yyyy"); } }
         public string Issue { get; set; }
         public string Description { get; set; }
         public int TimesOccured { get; set; }
