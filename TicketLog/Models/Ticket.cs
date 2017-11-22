054d908f43fd5a18a9bbcc9ffee0c5b11af382b7
kchiray <kchiray@my.yorku.ca>
2017-11-22 12:21:53 -0500

    auto: SubmissionDate (Tickets)

--- TicketLog/Models/Ticket.cs
+++ TicketLog/Models/Ticket.cs
@@ -8,7 +8,18 @@ namespace TicketLog.Models
     public class Ticket
     {           
         public int ID { get; set; }        
-        public String SubmissionDate { get { return DateTime.Now.ToString("dd/MM/yyyy"); } }
+        public DateTime SubmissionDate
+        {
+            get
+            {
+                DateTime submissionDate = new DateTime();
+                return submissionDate.Date;
+            }
+            set
+            {
+                SubmissionDate = value;
+            }
+        }
         public string Issue { get; set; }
         public string Description { get; set; }
         public int TimesOccured { get; set; }
