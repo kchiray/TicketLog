f83b0e85b00123172c0a2089537c10607a3de9cf
kchiray <kchiray@my.yorku.ca>
2017-11-22 21:27:50 -0500

    Field Authentication (required) - Tickets/Statistics

--- TicketLog/Models/Statistic.cs
+++ TicketLog/Models/Statistic.cs
@@ -9,13 +9,25 @@ namespace TicketLog.Models
     public class Statistic
     {
         public int Id { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
-        public DateTime EntryDate { get; set; }             
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Date")]
+        public DateTime EntryDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Amount of Incomplete Apps")]
         public int IncompleteApplications { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Amount of Emails")]
         public int Emails { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Amount of New Apps")]
         public int NewApplicaitons { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter How Many Apps To Complete")]
         public int ToDo { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Total Snowpass' Need to be Completed")]
         public int YearToDate { get; set; }
     }
 }
--- TicketLog/Models/Ticket.cs
+++ TicketLog/Models/Ticket.cs
@@ -9,12 +9,21 @@ namespace TicketLog.Models
     public class Ticket
     {           
         public int ID { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime SubmissionDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Issue")]
         public string Issue { get; set; }
+
         public string Description { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Times Issue Has Occured")]
         public int TimesOccured { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter Severity")]
         public string Severity { get; set; }
     }
 }
